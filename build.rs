0e96ecfbaa6017f1b74e09c1232a89e23e877243
Sebastian Imlay <sebastian.imlay@gmail.com>
2020-02-20 11:02:10 -0800

    Added modules

--- build.rs
+++ build.rs
@@ -61,7 +61,6 @@ fn build(sdk_path: Option<&str>, target: &str) {
     use std::env;
     use std::path::PathBuf;
 
-    let mut headers: Vec<String> = vec![];
     if !target.contains("apple-ios") {
         panic!("This project is only for ios")
     }
@@ -70,64 +69,68 @@ fn build(sdk_path: Option<&str>, target: &str) {
     // Get the cargo out directory.
     let out_dir = PathBuf::from(env::var("OUT_DIR").expect("env variable OUT_DIR not found"));
 
-    // Begin building the bindgen params.
-    let mut builder = bindgen::Builder::default();
-    builder = builder.objc_extern_crate(true);
-    // See https://github.com/rust-lang/rust-bindgen/issues/1211
-    // Technically according to the llvm mailing list, the argument to clang here should be
-    // -arch arm64 but it looks cleaner to just change the target.
-    let target = if target == "aarch64-apple-ios" {
-        "arm64-apple-ios"
-    } else {
-        target
-    };
-
-    builder = builder.clang_args(&[&format!("--target={}", target)]);
-
     if let Some(sdk_path) = sdk_path {
-        builder = builder.clang_args(&["-isysroot", sdk_path]);
 
         for framework in get_frameworks(sdk_path.clone()) {
             println!("cargo:rustc-link-lib=framework={}", framework);
+            // Begin building the bindgen params.
+            let mut builder = bindgen::Builder::default();
+            builder = builder.objc_extern_crate(true);
+            builder = builder.clang_args(&["-isysroot", sdk_path]);
+            // See https://github.com/rust-lang/rust-bindgen/issues/1211
+            // Technically according to the llvm mailing list, the argument to clang here should be
+            // -arch arm64 but it looks cleaner to just change the target.
+            let target = if target == "aarch64-apple-ios" {
+                "arm64-apple-ios"
+            } else {
+                target
+            };
+
+            builder = builder.clang_args(&[&format!("--target={}", target)]);
+
+            let mut headers: Vec<String> = vec![];
             headers.push(format!("{}/{}.h", framework, framework));
+            builder = builder.clang_args(&["-x", "objective-c", "-fblocks"]);
+            builder = builder.objc_extern_crate(true);
+            builder = builder.block_extern_crate(true);
+            builder = builder.generate_block(true);
+            builder = builder.rustfmt_bindings(true);
+
+            // time.h as has a variable called timezone that conflicts with some of the objective-c
+            // calls from NSCalendar.h in the Foundation framework. This removes that one variable.
+            builder = builder.blacklist_item("timezone");
+            builder = builder.blacklist_item("settimeofday");
+
+            // This function header has way too many parameters and seems to break the msg_esnd! macro
+            // https://developer.apple.com/documentation/metalperformanceshaders/mpssvgf/3143562-encodereprojectiontocommandbuffe?language=objc
+            builder = builder.blacklist_item("MPSSVGF");
+
+            // https://github.com/rust-lang/rust-bindgen/issues/1705
+            builder = builder.blacklist_item("ISCNCameraController");
+            builder = builder.blacklist_item("IUIStepper");
+            builder = builder.blacklist_item("objc_object");
+
+            let meta_header: Vec<_> = headers
+                .iter()
+                .map(|h| format!("#include <{}>\n", h))
+                .collect();
+
+
+            //builder = builder.header_contents("./test.h", &meta_header.concat());
+            builder = builder.header_contents(&format!("{}.h", framework), &meta_header.concat());
+
+            // Generate the bindings.
+            builder = builder.trust_clang_mangling(false).derive_default(true);
+
+            let bindings = builder.generate().expect("unable to generate bindings");
+
+            // Write them to the crate root.
+            bindings
+                .write_to_file(&format!("src/{}.rs", framework))
+                //.write_to_file(out_dir.join(&format!("{}.rs", framework)))
+                .expect("could not write bindings");
         }
     }
-    builder = builder.clang_args(&["-x", "objective-c", "-fblocks"]);
-    builder = builder.objc_extern_crate(true);
-    builder = builder.block_extern_crate(true);
-    builder = builder.generate_block(true);
-    builder = builder.rustfmt_bindings(true);
-
-    // time.h as has a variable called timezone that conflicts with some of the objective-c
-    // calls from NSCalendar.h in the Foundation framework. This removes that one variable.
-    builder = builder.blacklist_item("timezone");
-    builder = builder.blacklist_item("settimeofday");
-
-    // This function header has way too many parameters and seems to break the msg_esnd! macro
-    // https://developer.apple.com/documentation/metalperformanceshaders/mpssvgf/3143562-encodereprojectiontocommandbuffe?language=objc
-    builder = builder.blacklist_item("MPSSVGF");
-
-    // https://github.com/rust-lang/rust-bindgen/issues/1705
-    builder = builder.blacklist_item("UIStepper");
-    builder = builder.blacklist_item("SCNCameraController");
-
-    let meta_header: Vec<_> = headers
-        .iter()
-        .map(|h| format!("#include <{}>\n", h))
-        .collect();
-
-    //builder = builder.header_contents("./test.h", &meta_header.concat());
-    builder = builder.header_contents("Foundation.h", &meta_header.concat());
-
-    // Generate the bindings.
-    builder = builder.trust_clang_mangling(false).derive_default(true);
-
-    let bindings = builder.generate().expect("unable to generate bindings");
-
-    // Write them to the crate root.
-    bindings
-        .write_to_file(out_dir.join("all_bindings.rs"))
-        .expect("could not write bindings");
 }
 
 fn main() {
